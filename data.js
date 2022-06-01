60a7d6d31dd7a1d2e97281a1f5f0ed550140159b
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-06-01 23:32:03 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+     {
+          id: "jfhgbvnscs",
+          name: "Casual Shirt",
+          price: 45,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+          image: "images/img-1.jpg",
+     },
+     {
+          id: "ioytrhndcv",
+          name: "Office Shirt",
+          price: 100,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+          image: "images/img-2.jpg",
+     },
+     {
+          id: "wuefbncxbsn",
+          name: "T Shirt",
+          price: 25,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+          image: "images/img-3.jpg",
+     },
+     {
+          id: "thyfhcbcv",
+          name: "Mens Suit",
+          price: 300,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+          image: "images/img-4.jpg",
+     },
+];
